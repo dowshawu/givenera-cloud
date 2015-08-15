3b152e950cd33131700dcfbb64779be6bf7c94b4
dowshawu <dowshawu@gmail.com>
2015-08-15 10:28:23 -0500

    Fixed bug

--- cloud/main.js
+++ cloud/main.js
@@ -1,20 +1,18 @@
-require('cloud/post/emotions.js');
-require('cloud/post/compliments.js');
-require('cloud/relationship/relationship.js');
 require('cloud/post/getPost.js');
 require('cloud/post/posts.js');
 require('cloud/post/share.js');
 require('cloud/post/getDailyBread.js');
-require('cloud/search-user.js');
-require('cloud/user/profile-photo.js');
-require('cloud/postAutoIncrement.js');
-require('cloud/friend/getFriendList.js');
-require('cloud/map/getStatesHelpData.js');
+require('cloud/post/emotions.js');
+require('cloud/post/postAutoIncrement.js');
+require('cloud/post/compliments.js');
 require('cloud/post/getCategory.js');
 require('cloud/post/getPostImage.js');
-require('cloud/post/photos-crop.js')
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function (request, response) {
-  response.success("Hello world!");
-});
+
+require('cloud/relationship/relationship.js');
+
+require('cloud/friend/search-user.js');
+require('cloud/friend/getFriendList.js');
+
+require('cloud/user/profile-photo.js');
+
+
